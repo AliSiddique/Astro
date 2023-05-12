00936812a793db1a98e696dea9f3507217c4bdf0
AliSiddique <alisiddique10@hotmail.com>
2023-05-12 21:20:59 +0100

    first commit

---
+++ Solarsystem/Views/User/Auth/Login.swift
@@ -0,0 +1,20 @@
+//
+//  Login.swift
+//  Solarsystem
+//
+//  Created by Ali Siddique on 12/05/2023.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Login_Previews: PreviewProvider {
+    static var previews: some View {
+        Login()
+    }
+}
---
+++ Solarsystem/Views/User/Auth/Signup.swift
@@ -0,0 +1,20 @@
+//
+//  Signup.swift
+//  Solarsystem
+//
+//  Created by Ali Siddique on 12/05/2023.
+//
+
+import SwiftUI
+
+struct Signup: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Signup_Previews: PreviewProvider {
+    static var previews: some View {
+        Signup()
+    }
+}
